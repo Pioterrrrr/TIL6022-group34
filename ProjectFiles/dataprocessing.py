e3e30493a4ccf3586ddcae2d7172d69f9b6e2150
Pioterrrrr <ppreform@gmail.com>
2023-10-17 16:11:12 +0200

    framework for data processing

---
+++ ProjectFiles/dataprocessing.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import os
+datadir = 'data/'
+datalist = os.listdir(datadir)
+filepath = []
+
+# if filepath.endswith('xlsx'):
+# df = pd.read_excel(filepath)
+# elif filepath.endswith('csv'):
+# df = pd.read_csv(filepath)
+#else #tsv files
+# df = pd.read_csv(filepath,sep='\t')
+
+for file in datalist:
+    print(file)
+    filepath.append('data/' + file)
+
+print(filepath)
+
+for i in range(len(filepath)):
+    if i<=4:
+        print(filepath[i]+': NISZO+DAVIDE')
+        #write your code here
+    elif i == 5:
+        print(filepath[i]+': PIOTER')
+        # write your code here
+    elif i == 6:
+        print(filepath[i]+': ZACHOS')
+        # write your code here
+    elif i == 7:
+        print(filepath[i]+': VASILIS')
+        # write your code here
+    elif i == 8:
+        print(filepath[i]+': PIOTER')
+        # write your code here
+    elif i == 9:
+        print(filepath[i]+': PIOTER')
+        # write your code here
+
