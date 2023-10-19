75febd6f3a046898ea5fadb3194f279dbdbdd251
Pioterrrrr <ppreform@gmail.com>
2023-10-20 01:00:30 +0200

    aviation and rail dataframes are ready

--- ProjectFiles/dataprocessing.py
+++ ProjectFiles/dataprocessing.py
@@ -23,8 +23,14 @@ for i in range(len(filepath)):
         #write your code here
     elif i == 5:
         print(filepath[i]+': PIOTER')
+        # from 2018
         df_avia_pa = pd.read_csv(filepath[i])
-        print(df_avia_pa.head())
+        avia_countries = df_avia_pa['geo'].unique()
+        df_avia_pa_relevant = df_avia_pa.iloc[:,7:10]
+        df_avia_pa_relevant = df_avia_pa_relevant.rename({
+            'geo':'Geo','TIME_PERIOD':'Time_period','OBS_VALUE': 'Passengers_count'
+        }, axis='columns')
+        print(df_avia_pa_relevant.head())
     elif i == 6:
         print(filepath[i]+': ZACHOS')
         # write your code here
@@ -33,10 +39,18 @@ for i in range(len(filepath)):
         # write your code here
     elif i == 8:
         print(filepath[i]+': PIOTER')
+        # passenger count in thousands of passengers, from 2017 (columns go to 1997, but with no data)
         df_mar_pa = pd.read_csv(filepath[i],sep='\t')
-        print(df_mar_pa.head())
+        print(df_mar_pa.columns)
     elif i == 9:
         print(filepath[i]+': PIOTER')
+        # passenger count in thousands of passengers, from 2004
         df_rail_pa = pd.read_csv(filepath[i])
-        print(df_rail_pa.head())
+        rail_countries = df_rail_pa['geo'].unique()
+        df_rail_pa_relevant = df_rail_pa.iloc[:, 4:7]
+        print(df_rail_pa_relevant.columns[2])
+        df_rail_pa_relevant = df_rail_pa_relevant.rename({
+            'geo': 'Geo', 'TIME_PERIOD': 'Time_period', 'OBS_VALUE': 'Passengers_count'
+        }, axis='columns')
+        print(df_rail_pa_relevant.head())
 
