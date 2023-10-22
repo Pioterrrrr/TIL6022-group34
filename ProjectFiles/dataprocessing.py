3e7c01f6b0000634b33c718b003b0eafaab7a782
Pioterrrrr <ppreform@gmail.com>
2023-10-23 01:28:17 +0200

    almost finished passenger data processing

--- ProjectFiles/dataprocessing.py
+++ ProjectFiles/dataprocessing.py
@@ -23,7 +23,7 @@ for i in range(len(filepath)):
         print(filepath[i]+': NISZO+DAVIDE')
         #write your code here
     elif i == 5:
-        print(filepath[i]+': PIOTER')
+        print(filepath[i]+': PIOTR PIETRZAK')
         # from 2018
         df_avia_pa = pd.read_csv(filepath[i])
         avia_countries = df_avia_pa['geo'].unique()
@@ -39,12 +39,35 @@ for i in range(len(filepath)):
         print(filepath[i]+': VASILIS')
         # write your code here
     elif i == 8:
-        print(filepath[i]+': PIOTER')
-        # passenger count in thousands of passengers, from 2017 (columns go to 1997, but with no data)
+        print(filepath[i]+': PIOTR PIETRZAK')
+        # passenger count in thousands of passengers, from 2017
         df_mar_pa = pd.read_csv(filepath[i])
-        print(df_mar_pa.head())
+        df_mar_pa = df_mar_pa.iloc[:,6:9]
+        time_periods = df_mar_pa['TIME_PERIOD'].unique()
+        reporting_entities = df_mar_pa['rep_mar'].unique()
+        mar_countries = []
+        for r in reporting_entities:
+            country = r[:2]
+            if country not in mar_countries:
+                mar_countries.append(country)
+        df_mar_pa_relevant = pd.DataFrame(columns=df_mar_pa.columns)
+        # mar_sum = df_mar_pa[(df_mar_pa['TIME_PERIOD']=='2020-Q2')&(df_mar_pa['rep_mar'].str.startswith('BE'))].sum(numeric_only=True)
+        # print(mar_sum)
+        index_df_mar = 0;
+        for country in mar_countries:
+            for quarter in time_periods:
+                mar_sum = df_mar_pa[(df_mar_pa['TIME_PERIOD']==quarter)&(df_mar_pa['rep_mar'].str.startswith(country))].sum(numeric_only=True)
+                # print(mar_sum.iloc[0])
+                df_mar_pa_relevant = pd.concat([df_mar_pa_relevant,pd.DataFrame({'rep_mar':country,
+                                                                                 'TIME_PERIOD':quarter,
+                                                                                 'OBS_VALUE':mar_sum.iloc[0]},index=[index_df_mar])])
+                index_df_mar += 1
+        df_mar_pa_relevant = df_mar_pa_relevant.rename({
+            'rep_mar': 'Geo', 'TIME_PERIOD': 'Time_period', 'OBS_VALUE': 'Passengers_count'
+        }, axis='columns')
+        print(df_mar_pa_relevant.head())
     elif i == 9:
-        print(filepath[i]+': PIOTER')
+        print(filepath[i]+': PIOTR PIETRZAK')
         # passenger count in thousands of passengers, from 2004
         df_rail_pa = pd.read_csv(filepath[i])
         rail_countries = df_rail_pa['geo'].unique()
@@ -52,5 +75,5 @@ for i in range(len(filepath)):
         df_rail_pa_relevant = df_rail_pa_relevant.rename({
             'geo': 'Geo', 'TIME_PERIOD': 'Time_period', 'OBS_VALUE': 'Passengers_count'
         }, axis='columns')
-        # print(df_rail_pa_relevant.head())
+        print(df_rail_pa_relevant.head())
 
